31811ebefcbf480ca9d5acce1b4774427fdd739b
gregoriB <brandon.gregori@gmail.com>
2021-08-13 23:02:39 -0400

    Refactor and add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crypto_hash::{Algorithm, hex_digest};
+
 struct Block {
     value: &'static str,
     hash: String,
@@ -17,10 +18,8 @@ impl Block {
     }
 
     fn generate_hash(&mut self) {
-        let nonce_string = self.nonce.to_string();
-        let hash_string = nonce_string + self.value;
-        let hash = hex_digest(Algorithm::SHA256, hash_string.as_bytes());
-        self.hash = hash;
+        let hash_data = self.nonce.to_string() + self.value;
+        self.hash= hex_digest(Algorithm::SHA256, hash_data.as_bytes());
     }
 
     fn get_value(&self) -> &'static str {
@@ -32,29 +31,47 @@ impl Block {
     }
 
     fn mine(&mut self, difficulty: usize) {
-        let mut zeros: String = String::new();
-        for _i in 0..difficulty {
-            zeros.push('0');
-        }
+        let zeros = str::repeat("0", difficulty);
 
         let get_hash_slice= |hash: &str, size: usize| -> String {
-            let slice = &hash[0..size];
-            slice.to_owned()
+            hash[0..size].to_owned()
         };
 
-        while get_hash_slice(&self.hash, difficulty) != zeros.as_str() {
+        while get_hash_slice(&self.hash, difficulty) != zeros {
             self.generate_hash();
             self.nonce = self.nonce + 1;
         }
-        println!("Block successfully mined!");
+        println!("Block successfully mined");
     }
 }
 
 fn main() {
-    let mut block: Block = Block::new("block 1");
+    let mut block = Block::new("Block 1");
     println!("VALUE: {:?}", block.get_value());
-    block.mine(6);
+    block.mine(4);
     println!("HASH: {:?}", block.get_hash());
-    block.mine(6);
-    println!("HASH: {:?}", block.get_hash());
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Block;
+        
+    #[test]
+    fn block_instantiated_with_correct_properties() {
+        let name = "Test Block 1";
+        let block = Block::new(name);
+        assert_eq!(block.get_value(), name);
+        assert_eq!(block.get_hash().len(), 64);
+        assert_eq!(block.nonce, 0);
+    }
+
+    #[test]
+    fn block_is_mined() {
+        let mut block = Block::new("Test Block 2");
+        let difficulty = 3;
+        block.mine(difficulty);
+        let hash = block.get_hash();
+        assert_eq!(&hash[..difficulty], str::repeat("0", difficulty));
+        assert_eq!(hash.len(), 64);
+    }
 }
