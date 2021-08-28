ec59932fc4f9375f58369e2fb5aad2abbabbec0f
gregoriB <brandon.gregori@gmail.com>
2021-08-28 16:46:38 -0400

    Refactors to blockchain

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -33,27 +33,50 @@ impl BlockChain {
         self.chain.push(block);
     }
 
-    fn add_transaction(&mut self, transaction: Transaction) {
-        self.pending_transactions.push(transaction);
+    pub fn add_pending_transaction(&mut self, transaction: Transaction) {
+        let pending = &self.pending_transactions;
+        let mut pending_slice = pending[..].to_owned();
+        pending_slice.push(transaction);
+        self.pending_transactions = pending_slice;
     }
 
-    fn add_block(&mut self, payout_address: &'static str) {
-        let mut last_block = self.get_last_block();
-        let mut block = Block::new(self.pending_transactions.clone());
-        block.mine(self.difficulty);
-        last_block.set_next_hash(block.get_hash());
-        block.set_prev_hash(last_block.get_hash());
+    fn create_block(&self) -> Block {
+        Block::new(self.pending_transactions.clone())
+    }
+
+    fn pay_miner(&mut self, payout_address: &'static str) {
+        let payout_transaction = self.create_payout_transaction(payout_address);
+        self.add_pending_transaction(payout_transaction);
+    }
+
+    fn update_chain(&mut self, last_block: Block, new_block: Block) {
         let mut chain_slice = self.chain[0..self.chain.len() - 1].to_owned();
         chain_slice.push(last_block);
-        chain_slice.push(block);
+        chain_slice.push(new_block);
         self.chain = chain_slice;
-        let transaction = Transaction::new(self.name, payout_address, self.payout);
-        self.pending_transactions = vec![];
-        self.add_transaction(transaction);
     }
 
-    fn get_last_block(&self) -> Block {
+    pub fn add_block(&mut self, payout_address: &'static str) {
+        let mut last_block = self.get_last_block();
+        let mut new_block = self.create_block();
+        new_block.mine(self.difficulty);
+        last_block.set_next_hash(new_block.get_hash());
+        new_block.set_prev_hash(last_block.get_hash());
+        self.update_chain(last_block, new_block);
+        self.reset_pending_transactions();
+        self.pay_miner(payout_address);
+    }
+
+    fn create_payout_transaction(&self, payout_address: &'static str) -> Transaction {
+        Transaction::new(self.name, payout_address, self.payout)
+    }
+
+    pub fn get_last_block(&self) -> Block {
         self.chain.last().unwrap().to_owned()
+    }
+
+    fn reset_pending_transactions(&mut self) {
+        self.pending_transactions = vec![];
     }
 }
 
@@ -95,14 +118,14 @@ demonstrate! {
         }
 
         it "Adds a new transaction to pending transactions" {
-            blockchain.add_transaction(transaction.to_owned());
-            let pending = &blockchain.pending_transactions;
+            blockchain.add_pending_transaction(transaction.to_owned());
+            let pending = blockchain.pending_transactions;
             assert!(pending.len() == 1);
             assert_eq!(pending[0].get_sender(), transaction.get_sender());
         }
 
         it "Adds a new block to the chain" {
-            blockchain.add_transaction(transaction);
+            blockchain.add_pending_transaction(transaction);
             blockchain.add_block(payout_address);
             let prev_block = &blockchain.chain[0];
             let new_block = &blockchain.chain[1];
@@ -111,14 +134,14 @@ demonstrate! {
         }
 
         it "Pays miner after adding block to chain" {
-            blockchain.add_transaction(transaction);
+            blockchain.add_pending_transaction(transaction);
             blockchain.add_block(payout_address);
             let first_pending = blockchain.pending_transactions[0].to_owned();
             assert_eq!(first_pending.get_receiver(), payout_address);
         }
 
         it "Updates the previous block\'s next hash with the new block hash" {
-            blockchain.add_transaction(transaction);
+            blockchain.add_pending_transaction(transaction);
             blockchain.add_block(payout_address);
             let first_block = blockchain.chain.first().unwrap().to_owned();
             let last_block = blockchain.get_last_block();
